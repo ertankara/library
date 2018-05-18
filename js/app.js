9408083584be589a62a6437cd25ad12798f5c513
Ertan Kara <kara.ertan9@gmail.com>
2018-05-19 02:42:08 +0300

    Add responsive design

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-console.log('hi');
-if (12 > 5) {
-  console.log();
-}
-else {
-  console.log('Math is broken');
-}
+const hamburger = document
+  .querySelector('#hamburger')
+const aside = document
+  .querySelector('aside')
+const closeAsideButton = document
+  .querySelector('#close-aside-button')
+
+hamburger
+  .addEventListener('click', function() {
+    aside.style.transform = 'translateX(300px)'
+  })
+
+closeAsideButton
+  .addEventListener('click', function() {
+    aside.style.transform = 'translateX(-300px)'
+    console.log('close button')
+  })
+
